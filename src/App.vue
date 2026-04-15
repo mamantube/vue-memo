652864eff46b14731d9876aa60bfca400dfdf661
mamantube <firmanhiday.id007@gmail.com>
2026-04-15 17:25:24 +0700

    initial project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        <div class="container">
+            <header>
+                <h1>Memo</h1>
+                <button>+</button>
+            </header>
+            <div>[Card]</div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    height: 100vh;
+    width: 100vw;
+}
+
+.container {
+    max-width: 900px;
+    padding: 10px;
+    margin: 0 auto;
+}
+</style>
